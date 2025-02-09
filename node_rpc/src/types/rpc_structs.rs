e8c14f91858e7c9e44aaea53dfcc63cfa546c923
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2025-02-09 11:45:45 -0500

    rm node_rps redundunt code

--- node_rpc/src/types/rpc_structs.rs
+++ node_rpc/src/types/rpc_structs.rs
@@ -4,22 +4,7 @@ use rpc_primitives::parser::parse_params;
 use rpc_primitives::parser::RpcRequest;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use storage::block::Block;
-use storage::transaction::Transaction;
 use storage::transaction::TxKind;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct RegisterAccountRequest {}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SendTxRequest {
-    pub transaction: Transaction,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetBlockDataRequest {
-    pub block_id: u64,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ExecuteSubscenarioRequest {
@@ -37,12 +22,6 @@ pub struct ExecuteScenarioMultipleSendRequest {
     pub number_of_assets: usize,
     pub number_to_send: usize,
 }
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetGenesisIdRequest {}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetLastBlockRequest {}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ShowAccountPublicBalanceRequest {
@@ -111,14 +90,11 @@ pub struct WriteSplitUTXORequest {
     pub utxo_commitment: String,
 }
 
-parse_request!(RegisterAccountRequest);
-parse_request!(SendTxRequest);
-parse_request!(GetBlockDataRequest);
-parse_request!(GetGenesisIdRequest);
+// parse_request!(GetGenesisIdRequest);
 parse_request!(ExecuteSubscenarioRequest);
 parse_request!(ExecuteScenarioSplitRequest);
 parse_request!(ExecuteScenarioMultipleSendRequest);
-parse_request!(GetLastBlockRequest);
+// parse_request!(GetLastBlockRequest);
 
 parse_request!(ShowAccountPublicBalanceRequest);
 parse_request!(ShowAccountUTXORequest);
@@ -133,26 +109,6 @@ parse_request!(WriteSendDeshieldedBalanceRequest);
 parse_request!(WriteSplitUTXORequest);
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct HelloResponse {
-    pub greeting: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct RegisterAccountResponse {
-    pub status: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SendTxResponse {
-    pub status: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetBlockDataResponse {
-    pub block: Block,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct ExecuteSubscenarioResponse {
     pub scenario_result: String,
 }
@@ -165,16 +121,6 @@ pub struct ExecuteScenarioSplitResponse {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ExecuteScenarioMultipleSendResponse {
     pub scenario_result: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetGenesisIdResponse {
-    pub genesis_id: u64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetLastBlockResponse {
-    pub last_block: u64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
