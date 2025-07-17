de8462803d99739ff678e8436350d2df06342d5f
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2025-07-17 11:51:46 +0300

    fix: api rate limit refetch

--- sequencer_core/src/config.rs
+++ sequencer_core/src/config.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
+//
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 ///Helperstruct for account serialization
 pub struct AccountInitialData {
